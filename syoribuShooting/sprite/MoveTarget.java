4709bcb8cfe6c101dcdb59d7a116f69f264492c9
ydk-nk <ydk_nk@outlook.jp>
2018-10-03 17:11:46 +0900

    MoveTargetクラスを作成した(だけ)

---
+++ syoribuShooting/sprite/MoveTarget.java
@@ -0,0 +1,21 @@
+package syoribuShooting.sprite;
+
+import java.awt.image.BufferedImage;
+
+public class MoveTarget extends Target
+{
+
+    public MoveTarget(BufferedImage img, double centerX, double centerY)
+    {
+        super(img, centerX, centerY);
+        // TODO 自動生成されたコンストラクター・スタブ
+    }
+
+    @Override
+    public int getScore()
+    {
+        // TODO 自動生成されたメソッド・スタブ
+        return 0;
+    }
+
+}
